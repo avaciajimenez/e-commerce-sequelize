74c3462d898e91542b8004690d3f83d05170bd91
avacia jimenez <avaciajimenez@gmail.com>
2021-11-10 11:20:11 -0800

    routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,7 +3,7 @@ const { Category, Product, Tag } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
   try {
@@ -15,30 +15,32 @@ router.get('/', (req, res) => {
 
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async(req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
-  try {
-    const CategoryData = await Category.findByPk(req.params.id, {
-      include: [{ model: Product, through: Tag, as: 'categorys' }]
+  // try {
+  //   const CategoryData = await Category.findByPk(req.params.id, {
+  //     include: [{ model: Product, through: Tag, as: 'categorys' }]
+  //   } catch (err) {
+  //     res.status(500).json(err);
+  //   }
+  });
 
-});
-
-if (!locationData) {
-  res.status(404).json({ message: 'No category found with this id!' });
-  return;
-}
+// if (!locationData) {
+//   res.status(404).json({ message: 'No category found with this id!' });
+//   return;
+// }
 
 
-router.post('/', (req, res) => {
+router.post('/', async(req, res) => {
   // create a new category
-  router.post('/', async (req, res) => {
-    try {
-      const CategoryData = await Category.create(req.body);
-      res.status(200).json(CategoryData);
-    } catch (err) {
-      res.status(400).json(err);
-    }
+  // router.post('/', async (req, res) => {
+  //   try {
+  //     const CategoryData = await Category.create(req.body);
+  //     res.status(200).json(CategoryData);
+  //   } catch (err) {
+  //     res.status(400).json(err);
+  //   }
   
 });
 
@@ -46,24 +48,24 @@ router.put('/:id', (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async(req, res) => {
   // delete a category by its `id` value
-  try {
-    const CategoryData = await Category.destroy({
-      where: {
-        id: req.params.id
-      }
-    });
+  // try {
+  //   const CategoryData = await Category.destroy({
+  //     where: {
+  //       id: req.params.id
+  //     }
+  //   });
 
-    if (!CategoryData) {
-      res.status(404).json({ message: 'No category found with this id!' });
-      return;
-    }
+  //   if (!CategoryData) {
+  //     res.status(404).json({ message: 'No category found with this id!' });
+  //     return;
+  //   }
 
-    res.status(200).json(CategoryData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+  //   res.status(200).json(CategoryData);
+  // } catch (err) {
+  //   res.status(500).json(err);
+  // }
 
 });
 
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -23,6 +23,13 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new tag
+    try {
+      const Data = await Location.create(req.body);
+      res.status(200).json(locationData);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  
 });
 
 router.put('/:id', (req, res) => {
@@ -34,3 +41,6 @@ router.delete('/:id', (req, res) => {
 });
 
 module.exports = router;
+
+
+// week 13 mini project 28
